66fbe3b66dd4f0a2a991cc89aed9ba5dad7ad078
yaretzi27 <yaretzilopessanchez@gmail.com>
2026-08-13 21:07:34 -0600

    nuevo trabajos

--- 04-LenguajeEstrucruradodeConsultas/06-filtrado-registro-where.sql
+++ 04-LenguajeEstrucruradodeConsultas/06-filtrado-registro-where.sql
@@ -1,345 +1,340 @@
-/*==============================================
-DQL (DATA QUERY languaje ) en SQLSERVER
-archivo  06-filtado-registro-where.sql
-descripcion : se recupera unicamente las filas que cumplen determinadamnete 
-condiciones mediante la clausula where
-orden SINTACTICO 
-FROM 
-JOINS/ON 
-WHERE 
-GROUP BY
-HAVING 
-ORDER BY
+/* ============================================================================================
 
-ORDEN EJECUCION 
+DQL (Data Query Languaje) en SQLSERVER
 
-FROM/joins (INNER, LEFT, RIGH, CROSS, FULL, SELF)
-WHERE 
+Archivo: 06-filtrado-registros-where.sql
+
+
+Descripcion: Se recueran unicamente las filas que cumplen determinadas condiciones mediante la clausula where
+
+ORDEN SINTACTICO
+
+SELECT/ TOP/ DISTINCT
+FROM
+JOINS/ON
+WHERE
 GROUP BY
 HAVING
-SELECT 
+ORDER BY
+
+ORDEN DE EJECUCION 
+
+FROM / JOINS (INNER, LEFT, RIGHT, CROSS, FULL, SELF)
+WHERE
+GROUP BY
+HAVING
+SELECT
 DISTINCT
 ORDER BY
 TOP
-===========================================*/
 
-/*=============================================0
-SELect 
-columna_1,
-columna_2,
-columna_n,
-FROM nombre_tabla
-WHERE condicion,
+OPERADORES ARITMETICOS
+()
+*, /, %
++, -
 
-Nota: condicion pude ser relacionar  y a combinacion de esta con logica.
-Nota: El SELECT no filtra registros 
-===========================================*/
 
--- selcionar el´producto cuyo precio es $200
+OPERADORES RELACIONALES
+= Igual que
+< Menor que
+> Mayor que 
+<= Menor igual que
+>= Mayor igual que
+<> Diferente que 
+!= Diferente
 
+
+OPERADORES LOGICOS (boolean)
+NOT
+AND
+OR
+===============================================================================================*/
+
+
+/*==================================================================================================
+Sintaxis
+
+SELECT
+	columna_1, 
+	columna_2,
+	columna_n
+	FROM nombre_tabla
+	WHERE condicion;
+
+	Nota: condicion puede ser relacional y a combinacion de esta con logica 
+	Nota: El SELECT no filtra registros 
+
+===================================================================================================*/
+
+--Seleccionar el producto cuyo precio es $200
 SELECT 
-p.codigo AS [codigo],
-p.nombre AS [Producto],
-p.precio AS [Precio]
-From productos AS p
-WHERE precio=200;
+	p.codigo AS [Codigo]
+	p.nombre AS [Producto]
+	p.precio AS [Precio]
+FROM productos AS p;
+WHERE precio = 200;
 
----- Selecionar el cliente cuyo identificador es 25
-SELECT 
+--Seleccionar el cliente cuyo identificador es 25
+SELECT
 	c.id_cliente,
-	CONCAT (c.nombre, ' ',
-	c.apellido_paterno, ' ', 
-	c.apellido_materno) AS nombre:completo,
-	c.correo
-	FROM clientes AS c
-	WHERE c.id_cliente = 25
+	CONCAT (c.nombre, '', c.apellido_paterno, '', c.apellido_materno) AS nombre_completo,
+c.correo
+FROM clientes AS c 
+WHERE c.id_cliente = 25;
 
--- cpmparacion de cadenas de texto 
---- valores de texto deben escribirse
 
-SELECT 
-c.nombre AS [Categoria]
-fROM categoria  AS c
-WHERE c.nombre = 'computo';
+-- Comparacion de cadenas de texto 
+-- Los valores de texto deben escribirse entre comillas simples 
 
--- selecionar los datos del cliente con nombfre Cliente1
-SELECT 
+-- Seleccionar las categorias  
+SELECT
+	c.nombre AS [Categoria]
+FROM categorias AS c
+WHERE c.nombre = 'Computo';
+
+-- Seleccionar los datos del cliente con nombre Cliente1
+SELECT
 c.id_cliente,
-CONCAT (c.nombre. '' ,
-		c.apellido_paterno, ' ', 
+	CONCAT (c.nombre, '',
+	c.apellido_paterno, '',
 	c.apellido_materno) AS nombre_completo,
-	c.correo
-	FROM clientes AS c
-	WHERE  nombre = 'CLIENTE1'
--- selciionra los datos del empleado que no pertenesca al departemento 1
-SELECT 
-e.id_empleado,
+c.correo
+FROM cliente AS c;
+WHERE nombre = 'Cliente1';
+
+-- Seleccionar los datos del empleado que no pertenezcan al departamento 1
+SELECT
 e.nombre,
 e.id_departamento,
-e.salrio,
-FROm empleados AS 
--- selccionar los datos de los productos donde el precio sea superior a $490
-SELECT 
-p.id_producto,
-p.nombre,
-p.id_categoria,
+e.salario
+FROM empleados AS e;
+WHERE id_departamento =1;
+GO
 
--- selecionat los datos del producto con exixtencia  critica inferir a 
----10 unidaees
-SELECT 
-p.id_producto,
-p.nomvre,
-p.id_categoria,
+-- Seleccionar los datos de los productos deonde el precio sea superior a $490
+SELECT
+	p.id_producto,
+	p.nombre,
+	p.id_categoria,
+	p.precio
+	FROM producto AS p
+	WHERE p.precio 
+FROM productos
+WHERE existencia<10;
 
--- selecionar los datos de los empleados donde su salario sea de $30,000 en 
--- addlante 
+-- Seleccionar los datos de los productos con existencia critica inferior a 10 unidades
+
+-- Seleccionar los datos de los empleados donde su salario sea de $30,000 en adelante
 SELECT 
-E.id_empleado,
-e.nombre,
-e.id_
---- selcionar los datos  de los productos donde sus precios sean de $10 o menos
+	e.id_empleado,
+	e.nombre,
+	e.id_departamento,
+	e.salario
+FROM emlpleado AS e
+WHERE salario >= 30000
+
+-- Seleccionar los datos de los productos donde sus precios sean de $10 o menos 
 SELECT 
-p.id_producto,
-p.nombre,
-p.id_categoria,
-p.precio
+	p.id_producto,
+	p.nombre,
+	p.id_categoria,
+	p.precio
 FROM productos AS p
-WHERE p.precio <=10;
--- comparacion de fechas
---- las fechas deben escribirse entre comillas simples
- -- selciionar 
-SELECT 
-v.id_venta,
-v.fecha,
-v.id_cliente,
-v.id_empleado
-FROM ventas AS v
-WHERE fecha = '2025-12-24';
+WHERE p.precio <=10
+-- Comparacion de fechas
+-- Las fechas deben escribirse ebtre comillas simples 
 
--- selciionar los datos de las ventas realizadas en el 2025
+-- Se recomienda el formato AAAA-MM-DD
 
-SELECT 
-v.id_venta,
-v.fecha,
-YEAR(v.fecha) AS [año],
-FORMAT (v.fecha , 'mmm') AS [mes_ingles],
-  FORMAT (v.fecha) AS [dia]
-v.id_cliente,
-v.id_empleado
-FROM ventas AS v
+-- Seleccionar los datos de las ventas realizadas el 24 de diciembre de 2025
+
+SELECT
+	v.id_venta,
+	v.fecha,
+	v.id_cliente,
+	v.id_empleado
+FROM ventas as v 
+WHERE fecha = '2025-12-24'
+
+-- Seleccionar los datos de las ventas realizadas en 2025
+
+SELECT
+	v.id_venta,
+	v.fecha,
+	YEAR(v.fecha) AS [A�O], 
+	FORMAT(v.fecha, 'MMMM') AS [mes_ingles],
+	FORMAT(v.fecha, 'MMMM', 'es-ES') AS [mes_espa�ol],
+	DAY(v.fecha) AS [dia],
+	FORMAT(v.fecha, 'dddd') AS [mes_ingles],
+	UPPER(FORMAT (v.fecha, 'dddd', 'es-ES')) AS [dia_espa�ol],
+	UPPER(FORMAT (v.fecha, 'ddd', 'es-ES')) AS [mes_abreviatura],
+	v.id_cliente,
+	v.id_empleado
+FROM ventas as v 
 WHERE MONTH (fecha) = 4;
 
--- selecionar todas las ventas anteriores al 1 de febrero de 2025
+-- MOSTRAR LOS PRODUCTOS CON PRECIO ENTRE $200 Y $300 QUE ADEMAS TENGAN MENOS DE cincuenta unidades
 
----DISTINCT 
---- qiuta elementos repetidos de una o la combinacion de columnas {
--- muestrame los sexos de los clientes 
-
-SELECT DISTINCT 
-sexo 
-FROM clientes AS  c;
-
-SELECT DISTINCT 
-id_ciudad
-FROM clientes  AS c;
-
-
-SELECT DISTINCT 
-cu.nombre;.
-FROM clientes AS c
-INNER JOIN ciudades as cu
-on c,id_cuidado = cu.id_ciudad;
-
--- seleciionar los desciuentos unicos de las ventasn
-
-SELECT 
-dv.descuento
-FROM detalle_venta AS dv
-ORDER BY  dv.descuento DEC;
-
----tOP 
----limita la cantidad de filas devueltas por una consulta
-SELECT TOP(20)
-dv.id_venta
-dv,precio,
-dv.cantidad,
-dv.descuento,
-FROM detalle_venta as dv;
-
----selecionar los datos de los productos mostrando el codigo y el valor 
--- del inventario, donde el valor del inventario debe ser mayor a 50000
-
-SELECT 
-p.codigo as codigo_producto,
-p.existencia AS existencia,
-p.precio AS precio, 
-(p.precio * p.exisitencia) AS valor_inventario
-From productos AS p 
-WHERE (p.precio * p.existencia) > 500;
-
--- mostrar los productos con precio que esten entre $200 y $300
-
-
-SELECT 
-p.codigo as codigo_producto,
-p.existencia AS existencia,
-p.precio AS precio, 
-(p.precio * p.exisitencia) AS valor_inventario
-From productos AS p 
-WHERE precio >=200 AND precio <=300;
--- INSTRUCCION BETWEEN
-
-SELECT 
-p.codigo as codigo_producto,
-p.existencia AS existencia,
-p.precio AS precio, 
-(p.precio * p.exisitencia) AS valor_inventario
-From productos AS p 
-WHERE precio BETWEEN 200 AND 300;
-
---- -- mostrar los productos con precio que esten entre $200 y $300 que ademas
--- de cincuenta unidades
-
-SELECT 
-p.codigo,
-p.nombre,
-p.precio,
-p.existencia
-FROM productos AS p;
-WHERE p.precio>=200
-		and p.precio<=300
-		AND 
-		p.existencia < 50
-ORDER BY precio DESC;
-
--- mostrar      productos existencia inferior a 10 superior a 190
-
---- ORDENADOR NOT 
-
-SELECT 
-p.codigo,
-p.nombre,
-p.precio,
-p.existencia
+SELECT
+		p.codigo,
+		p.nombre,
+		p.precio,
+		p,existencia
 FROM productos AS p
-WHERE p.existencia  10
-		
-	OR 
-		p.existencia >190;
+WHERE p.precio >=200;
+	AND
+	p.precio<=300
+	AND
+	p.existencia <50
+ORDER BY precio DESC
 
-
-
---- ORDENADOR NOT 
-
--- mostrar los productos que no sean mayores a 400
-
+--Mostrar productos con existencia inferior a 10 o superior a 190
 SELECT 
-p.codigo,
-p.nombre,
-p.precio,
-p.existencia
-FROM productos AS p
-WHERE NOT p.precio>400;
+	p.codigo,
+	p.nombre,
+	p.precio,
+	p.existencia
+FROM productos AS p 
+WHERE p.existencia<10
+	OR
+	p.existencia>190
+
+	--OPERADOR NOT
+
+	--Mostrar los productos que su precio no sea mayor a 400
+	SELECT 
+	p.codigo,
+	p.nombre,
+	p.precio,
+	p.existencia
+FROM productos AS p 
+WHERE NOT p.precio <= 400;
 
 
--- mostrar los empleados de los departamentos uno y dos que tengan salario mayor 
---- a $25,000
-SELECT 
-e.id_empledo,
-e.nombre,
-e.salario,
-e.id_departamento
-FROM empleado AS e
-WHERE(e.departamento =1
-or
-e.id_departamento = 2)
-AND e.salario >25000;
+-- Mostrar los empleados de los departamentos 1 y 2 que tengan salario mayor a 25,000
+SELECT
+	e.id_empleado
+	e.nombre,
+	e.salario,
+	e.id_departamento
+FROM empleados AS e
+WHERE (e.id_departamento=1
+	OR
+	e.id_departamento = 2)
+	AND e.salario >25000;
+;
 
---- mostrar los empledos qie no tiene jefe 
-SELECT 
-p.id_empledos,
-p.id_departamneto,
-p.nombre,
-p.id_jefe
+--Mostrar los empleados que no tienen jefe 
+SELECT
+	p.id_empleado,
+	p.id_departamento,
+	p.nombre,
+	p.id_jefe 
 FROM empleados AS p
-WHERE P.id_jefe IS NULL;
+WHERE p.id_jefe IS NOT NULL;
 
--- OPERADOR BETWEN
---permite comprobar si un valor se encuentan dentro de un frango inclusivo 
+-- OPERADOR BETWEEN
+-- Permite comprobar si un valor se encuentra dentro de un rango inclusivo
 
-/* =====================================================
-WHERE columna BETWEEN limite_inferior AND limite superior
-===========================================================*/
+/*==================================================================
+WHERE columna BETWEEN limite_inferior AND limite_superior
+====================================================================*/
 
---- Mostrar empledos con salario entre $15,000 y $20,000 incluyendo ambos limites 
-SELECT 
-e.id_empeado,
-e.nombre,
-e.salario
-FROM empleados AS e 
+-- Mostrar empleados con salario entre $15,000 y $20,000, incluyendo ambos limites
+
+SELECT
+    e.id_empleado,
+    e.nombre,
+    e.salario
+FROM empleados AS e
 WHERE salario BETWEEN 15000 AND 20000
 ORDER BY 3 DESC;
 
-SELECT 
-e.id_empeado,
-e.nombre,
-e.salario
-FROM empleados AS e 
-WHERE e.slario >=15000
-		and 
-		e.salario<=20000;
+SELECT
+    e.id_empleado,
+    e.nombre,
+    e.salario
+FROM empleados AS e
+WHERE e.salario>=15000
+      AND
+      e.salario<=20000;
 
----  mostrar los´productos que los precios esten en el rango de 100 o 200
-SELECT 
-p.codigo,
-p.nombre,
-p.precio,
-p.existencia
+-- Mostrar los productos que los precios esten en el rango de 100 y 200
+SELECT
+    p.codigo,
+    p.nombre,
+    p.precio,
+    p.existencia
 FROM productos AS p
-WHERE p.precio BETWEEN	100 AND 200;
-
--- mostrar las ventas del 1 de enero del 2025 al 10 de enro del 2025
-SELECT 
-v.id_venta AS [numero_venta],
-v.id_cliente AS [cliente],
-v.id_empledo AS [vendedor],
-v.fecha AS [fecha_venta],
-UPPER (FORMAT(V.fecha 'm'. 'es_ES')) AS [mes_venta],
-UPPER (FORMAT (v.fecha, 'dddd', 'es_ES')) AS [dia_venta],
-DATEPART(YEAR, v.fecha) AS [año_venta]
+WHERE p.precio BETWEEN 100 AND 200;
+-- Mostrar las ventas del 1 de enero de 2025 al 10 de enero de 2025
+SELECT
+    v.id_venta AS [numero_venta],
+    v.id_cliente AS [cliente],
+    v.id_empleado AS [vendedor],
+    v.fecha AS [fecha_venta],
+    UPPER(FORMAT(v.fecha, 'MMMM', 'es-ES')) AS [mes_venta],
+    UPPER(FORMAT(v.fecha, 'dddd', 'es-ES')) AS [dia_venta],
+    DATEPART(YEAR, v.fecha) AS [año_venta]
 FROM ventas AS v
 WHERE v.fecha BETWEEN '2025-01-01' AND '2025-01-10'
 ORDER BY [cliente] ASC;
 
+-- Mostrar los productos que su rango de precios no este entre 100 y 400
 
--- mostra los productos que su rango de precio no este entre 100 y 400
-SELECT 
-p.codigo,
-p.nombre,
-p.precio,
-p.existencia
+SELECT
+    p.codigo,
+    p.nombre,
+    p.precio,
+    p.existencia
 FROM productos AS p
-WHERE p.precio AS p
 WHERE p.precio NOT BETWEEN 100 AND 400;
 
----- operador IN 
----permite comparar una columna con una lista DE valore
-/*========================================================00
-WHERE  columna IN(valor_1, valor_2, valor_3)
-=============================================*/
--- mostrar productos pertenencientes a las categorias 1, 7, o 12
-SELECT 
-p.codigo,
-p.nombre,
-p.precio,
-p.existencia
-p.id_categoria
+-- OPERADOR IN
+-- permite comparar una columna con una lista de valores
+
+/*==================================================================
+WHERE columna IN (valor_1, valor_2, valor_3)
+====================================================================*/
+
+-- Mostrar productos pertenecientes a las categorias 1,7 o 12
+
+SELECT
+    p.codigo,
+    p.nombre,
+    p.precio,
+    p.existencia,
+    p.id_categoria
 FROM productos AS p
-WHERE p.id_categoria = 1
-OR p.id_categoria=7
-OR p.id_categoria =12
-ORDER BY	p.id_categoria;
-/*==========================================000 TODO OPERADOR LIKE ==========
+WHERE p.id_categoria IN (1,7,12)
+ORDER BY p.id_categoria;
+
+
+SELECT
+    p.codigo,
+    p.nombre,
+    p.precio,
+    p.existencia,
+    p.id_categoria
+FROM productos AS p
+WHERE p.id_categoria  = 1
+      OR p.id_categoria = 7
+      OR p.id_categoria = 12
+ORDER BY p.id_categoria;
+
+-- Mostrar todos los productos que no pertenecen a la categoria 1, 7 o 12
+
+SELECT
+    p.codigo,
+    p.nombre,
+    p.precio,
+    p.existencia,
+    p.id_categoria
+FROM productos AS p
+WHERE p.id_categoria NOT IN (1,7,12)
+ORDER BY p.id_categoria;
+
+/*==========================================TODO OPERADOR LIKE ==========
 permite buscar patrones dentro de valores de texto
 
 sintaxis 
